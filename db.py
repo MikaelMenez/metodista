0d7b528063a18622dc7895ba0c994992c88e6e0f
Mikael Menezes da Rocha Barreto <mixaelmenezes@gmail.com>
2025-09-28 19:26:46 -0300

    início

---
+++ db.py
@@ -0,0 +1,82 @@
+import sqlite3
+
+
+def create_eventos_table():
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS  eventos(id INTEGER PRIMARY KEY AUTOINCREMENT,data TEXT NOT NULL,usuario TEXT NOT NULL)")
+def create_user_table():
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,senha TEXT NOT NULL,usuario TEXT NOT NULL,tipo TEXT NOT NULL)")
+def insert_user(senha:str,usuario:str,tipo:str):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("INSERT INTO users (senha,usuario,tipo) VALUES (?,?,?)",(senha,usuario,tipo))
+    connection.commit()
+def insert_eventos(data:str,usuario:str):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("INSERT INTO eventos (data,usuario) VALUES (?,?)",(data,usuario))
+    connection.commit()
+
+def modify_user(id:int,senha:str,usuario:str,tipo:str):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("UPDATE users SET data=? , usuario=?,tipo=? WHERE id=?",(senha,usuario,tipo,id))
+    connection.commit()
+def modify_eventos(id:int,data:str,usuario:str):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("UPDATE eventos SET data=? , usuario=? WHERE id=?",(data,usuario,id))
+    connection.commit()
+def delete_eventos(id:int):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("DELETE FROM eventos WHERE id=?",(id,))
+    connection.commit()
+def delete_user(id:int):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("DELETE FROM users WHERE id=?",(id,))
+    connection.commit()
+def get_eventos(id:int):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM eventos WHERE id=?",(id,))
+    connection.commit()
+    data=cursor.fetchall()
+    return data
+def get_user(id:int):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE id=?",(id,))
+    connection.commit()
+    data=cursor.fetchall()
+    return data
+def get_user(usuario:str,senha:str):
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE usuario=? AND senha=?",(usuario,senha))
+    connection.commit()
+    data=cursor.fetchall()
+    return data
+
+def get_all_eventos():
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM eventos")
+    connection.commit()
+    data=cursor.fetchall()
+    return data
+
+def get_all_users():
+    connection=sqlite3.connect('datas.db')
+    cursor=connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    connection.commit()
+    data=cursor.fetchall()
+    return data
+
+create_eventos_table()
+create_user_table()
---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI,Request,Form
+from fastapi.responses import HTMLResponse,JSONResponse,RedirectResponse
+from fastapi.templating import Jinja2Templates
+import calendar
+import datetime
+from db import *
+
+app=FastAPI()
+templates=Jinja2Templates(directory="templates")
+
+@app.get('/index.html')
+def landing_page(request:Request):
+    return templates.TemplateResponse('index.html',context={"request":request})
+@app.get("/calendar",response_class=HTMLResponse)
+async def calendar(request:Request):
+   
+    return templates.TemplateResponse(name="calendar.html",request=request)
+@app.get("/signin",response_class=HTMLResponse)
+def register(request:Request):
+    return templates.TemplateResponse('/signin.html',context={'request':request})
+@app.get('/log_in',response_class=HTMLResponse)
+def log_in(request:Request):
+    return templates.TemplateResponse('login.html',context={'request':request,"login_error":False})
+@app.post("/login")
+def login(request:Request,email:str=Form(...),senha:str=Form(...)):
+    try:
+        get_user(email,senha)
+        return RedirectResponse('/calendar') 
+    except Exception as e:   
+        print(e)
+        return templates.TemplateResponse('login.html',context={'request':request,'login_error':True})
+@app.post("/register",response_class=HTMLResponse)
+def register(request:Request,senha:str= Form(...),email:str =Form(...),nome:str=Form(...)):
+    insert_user(senha=senha,usuario=email,tipo='usuario')
+    return RedirectResponse('/index.html')
+@app.get("/eventos")
+async def get_eventos():
+    eventos=get_all_eventos()
+    lista_eventos=[]
+    for evento in eventos:
+        lista_eventos.append({
+            'id':evento[0],
+            'start':evento[1],
+            'title':evento[2]
+        })
+    print(lista_eventos)
+    return JSONResponse(lista_eventos)
